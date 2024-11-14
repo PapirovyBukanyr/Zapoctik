046b33abb845b605ed7752e10222a85eb7e0819d
PapirovyBukanyr <101704235+PapirovyBukanyr@users.noreply.github.com>
2024-11-14 18:38:18 +0100

    Dokumentace a final fix

--- GameView.py
+++ GameView.py
@@ -21,6 +21,11 @@ class ClickableLabel(QLabel):
 
 class GameView(QWidget):
     def __init__(self, game):
+        """Konstruktor
+
+        Args:
+            game (game): Hra, kterou chceme zobrazit
+        """
         super().__init__()
         self.setWindowTitle(game.__str__())
         self.setGeometry(100, 100, 400, 400)  
@@ -34,6 +39,13 @@ class GameView(QWidget):
         self.update_board(True)
                     
     def set_piece_image(self, row, col, filePath):
+        """Funkce pro nastavení obrázku figurky na dané pozici
+
+        Args:
+            row (int): řádek
+            col (int): sloupec
+            filePath (string): cesta k obrázku
+        """
         if 0 <= row < len(self.game.getBoard()) and 0 <= col < len(self.game.getBoard()[0]):  
             label = self.uiBoard[row][col]
             pixmap = QPixmap(filePath)
@@ -41,6 +53,8 @@ class GameView(QWidget):
             label.setAlignment(Qt.AlignCenter)
 
     def create_board(self):
+        """Funkce pro vytvoření herní desky
+        """
         if not self.board_layout:
             self.board_layout = QGridLayout()
             self.layout.addLayout(self.board_layout)
@@ -63,38 +77,73 @@ class GameView(QWidget):
                 self.uiBoard[row][col] = label
  
     def handle_square_click(self, row, col):
+        """Funkce pro obsluhu kliknutí na políčko
+
+        Args:
+            row (int): řádek
+            col (int): sloupec
+        """
         if self.selectedPiece == False and (isinstance(self.game, Checkers) or isinstance(self.game, Chess) or isinstance(self.game, MathGame)):
-            self.update_board()
-            self.possible_moves = self.game.choosePiece([row, col], self.player)
-            for move in self.possible_moves:
-                self.highlight_square(move[0], move[1])
-            self.selectedPiece = True
+            self.choose_piece(row, col)
         else:
-            result = self.game.makeMove([row, col], self.player)
-            if result == "Promote":
-                self.promote_pawn()
-            elif result == False:
-                self.selectedPiece = False
-                self.update_board()   
-                return
-            elif result == True:
-                self.selectedPiece = False
-                self.update_board()
-            else:
-                self.update_board()
-                for move in result:
-                    self.highlight_square(move[0], move[1])
-                return
-            if self.game.checkEnd() != None:
-                self.game_ended(self.game.checkEnd())
-            self.player = self.player.changeColor()
+            self.make_move(row, col)
+    
+    def choose_piece(self, row, col):
+        """Funkce pro výběr figurky
+        
+        Args:
+            row (int): řádek
+            col (int): sloupec
+        """
+        self.update_board()
+        self.possible_moves = self.game.choosePiece([row, col], self.player)
+        for move in self.possible_moves:
+            self.highlight_square(move[0], move[1])
+        self.selectedPiece = True
+        
+    def make_move(self, row, col):
+        """Funkce pro provedení tahu
+        
+        Args:
+            row (int): řádek
+            col (int): sloupec
+        """
+        result = self.game.makeMove([row, col], self.player)
+        if result == "Promote":
+            self.promote_pawn()
+        elif result == False:
+            self.selectedPiece = False
+            self.update_board()   
+            return
+        elif result == True:
+            self.selectedPiece = False
+            self.update_board()
+        else:
+            self.update_board()
+            for move in result:
+                self.highlight_square(move[0], move[1])
+            return
+        if self.game.checkEnd() != None:
+            self.game_ended(self.game.checkEnd())
+        self.player = self.player.changeColor()
     
     def highlight_square(self, row, col):
+        """Funkce pro zvýraznění políčka
+        
+        Args:
+            row (int): řádek
+            col (int): sloupec
+        """
         label = self.uiBoard[row][col]
         label.setStyleSheet("background-color: yellow;")
         label.isHighlighted = True
 
-    def update_board(self, isFirst = True):
+    def update_board(self, isFirst = False):
+        """Funkce pro aktualizaci herní desky
+        
+        Args:
+            isFirst (bool, optional): Je to první aktualizace?. Defaults to False.
+        """
         if not isFirst: 
             self.remove_board()
         self.uiBoard = [[None for _ in range(len(self.game.getBoard()))] for _ in range( len(self.game.getBoard()[0]))]
@@ -105,6 +154,8 @@ class GameView(QWidget):
                     self.set_piece_image(i, j, GetResource.getResource(self.game.getBoard()[i][j]))
 
     def remove_board(self):
+        """Funkce pro odstranění herní desky
+        """
         for row in range(len(self.game.getBoard())):
             for col in range( len(self.game.getBoard()[0])):
                 widget = self.uiBoard[row][col]
@@ -115,6 +166,11 @@ class GameView(QWidget):
         gc.collect()
 
     def game_ended(self, message):
+        """Funkce pro zobrazení dialogového okna s výsledkem hry
+        
+        Args:
+            message (string): Výsledek hry
+        """
         if isinstance(self.game, TicTacToe):
             if message == "B won":
                 message = "Vyhrál hráč O"
@@ -131,10 +187,13 @@ class GameView(QWidget):
                 message = "Remíza"
         msg_box = QMessageBox()
         msg_box.setText(message)
+        msg_box.setWindowTitle("Konec hry")
         msg_box.exec_()
         self.close()
         
     def promote_pawn(self):
+        """Funkce pro výběr figurky, na kterou se má pesák změnit
+        """
         items = ["Dáma", "Věž", "Střelec", "Kůň", "Král"]
         item, ok = QInputDialog.getItem(self, "Promote Pawn", "Select piece to promote to:", items, 0, False)
         
