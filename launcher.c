712edc6947905cd8efd271b5777b8c434a4d4a63
PapirovyBukanyr <101704235+PapirovyBukanyr@users.noreply.github.com>
2024-11-24 19:54:55 +0100

    Odstranění diakritiky, která v terminálu blbla

--- launcher.c
+++ launcher.c
@@ -1,32 +1,42 @@
 #include <stdlib.h> 
 #include <stdio.h>  
 
+int call_pyc_killer_3000() {
+
+    printf("Spoustim skript pyc_killer.exe...\n");
+
+    if (system("pyc_killer.exe") != 0) {
+        printf("Chyba pri spousteni skriptu pyc_killer.exe!\n");
+        return 1; 
+    }
+    
+    printf("Genocida provedena.\n");
+
+    return 0; 
+}
+
 int main() {
-    printf("Instalace požadovaných Python knihoven...\n");
+    printf("Instalace pozadovanych Python knihoven...\n");
     
     if (system("pip install sympy numpy PyQt5 PyQtWebEngine") != 0) {
-        printf("Chyba při instalaci knihoven!\n");
+        printf("Chyba pri instalaci knihoven!\n");
         return 1; 
     }
 
-    printf("Knihovny úspěšně nainstalovány.\n");
+    printf("Knihovny uspesne nainstalovany.\n");
 
-    printf("Spouštím skript main.py...\n");
+    printf("Spoustim skript main.py...\n");
     if (system("python main.py") != 0) {
-        printf("Chyba při spouštění skriptu main.py!\n");
-        return 1;
+        printf("Chyba pri spousteni skriptu main.py!\n");
+        call_pyc_killer_3000();
+        return 2;
     }
 
-    printf("Skript main.py byl úspěšně spuštěn.\n");
+    printf("Skript main.py byl uspesne spusten.\n");
 
-
-    printf("Mazání stop za pomocí luxusního pyc killeru 3000...\n");
-    if (system("pyc_killer.exe") != 0) {
-        printf("Chyba při spouštění skriptu pyc_killer.exe!\n");
-        return 1; 
+    if (call_pyc_killer_3000() != 0) {
+        return 3; 
     }
-
-    printf("Genocida provedena.\n");
 
     return 0; 
 }
