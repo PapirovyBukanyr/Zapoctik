ed5a481cb5377bc7868d55e3bf13a0979b65a51d
PapirovyBukanyr <101704235+PapirovyBukanyr@users.noreply.github.com>
2024-11-24 19:57:04 +0100

    Přidání komentářů

--- launcher.c
+++ launcher.c
@@ -1,6 +1,12 @@
 #include <stdlib.h> 
 #include <stdio.h>  
-
+/** 
+ * Spusti skript pyc_killer.exe, ktery odstrani vsechny soubory s priponou .pyc v adresari a jeho podadresarich.
+ * 
+ * @return 0 pokud byl skript uspesne spusten
+ * @return 1 pokud nastala chyba pri spousteni skriptu
+ *
+*/
 int call_pyc_killer_3000() {
 
     printf("Spoustim skript pyc_killer.exe...\n");
@@ -15,6 +21,14 @@ int call_pyc_killer_3000() {
     return 0; 
 }
 
+/**
+ * Instaluje potrebne Python knihovny a spusti skript main.py.
+ * 
+ * @return 0 pokud vse probehlo v poradku
+ * @return 1 pokud nastala chyba pri instalaci knihoven
+ * @return 2 pokud nastala chyba pri spousteni skriptu main.py
+ * @return 3 pokud nastala chyba pri spousteni skriptu pyc_killer.exe
+ */
 int main() {
     printf("Instalace pozadovanych Python knihoven...\n");
     
