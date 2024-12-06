d50d17e7968b545bc1d7318edf1133f148cba096
Vojtěch Hora <vojtahora12@gmail.com>
2024-12-06 18:25:56 +0100

    rychla oprava

--- MainView.py
+++ MainView.py
@@ -94,6 +94,6 @@ class MainView(QWidget):
         Args:
             game (Game): objekt Game, který obsahuje název hry a objekt hry
         """
-        self.gameWindow = GameView(game.game, game.name)
+        self.gameWindow = GameView(game.game)
         self.gameWindow.show()
         self.showMinimized()
